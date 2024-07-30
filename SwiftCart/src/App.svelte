0c0ebec8ab4316672889f8be88c778e374696892
Phillip Bogopane <pm.bogopane@gamil.com>
2024-07-30 16:50:52 +0200

    Created files

---
+++ SwiftCart/src/App.svelte
@@ -0,0 +1,18 @@
+<script>
+  import { Router, Link, Route } from 'svelte-routing';
+  import Navbar from './Navbar.svelte';
+  import ProductList from './ProductList.svelte';
+  import ProductDetails from './ProductDetails.svelte';
+
+  let products = [];
+  let categories = [];
+</script>
+
+<Navbar />
+
+<Router>
+  <Route path="/" component={ProductList} />
+  <Route path="product/:productId" let:params>
+      <ProductDetails productId={params.productId} />
+  </Route>
+</Router>
